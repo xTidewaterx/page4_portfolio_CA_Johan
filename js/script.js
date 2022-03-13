779ff5ef3146c97a73246c2f0ac22ca43b81a0ce
xTidewaterx <79268288+xTidewaterx@users.noreply.github.com>
2022-03-13 23:52:07 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const buttonA = document.querySelector("button.a");
+const pA = document.querySelector("p.a");
+
+
+const buttonB = document.querySelector("button.b");
+const pB = document.querySelector("p.b");
+
+const buttonC = document.querySelector("button.c");
+const pC = document.querySelector("p.c");
+
+
+
+
+function showText1 () {
+    buttonA.style.display ="none";
+    pA.style.display =  "inline-block";
+
+}
+
+
+function showText2 () {
+    buttonB.style.display ="none";
+    pB.style.display =  "inline-block";
+
+}
+
+
+function showText3 () {
+    buttonC.style.display ="none";
+    pC.style.display =  "inline-block";
+
+}
+
+
